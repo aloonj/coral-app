1eac2613cdd25c2a4d50769722dfee8d5a1afeef
Alun <alunjames@gmail.com>
2025-05-04 15:01:38 +0100

    scopes

--- backend/src/services/xeroService.js
+++ backend/src/services/xeroService.js
@@ -27,7 +27,7 @@ const initXero = () => {
       clientId: process.env.XERO_CLIENT_ID,
       clientSecret: process.env.XERO_CLIENT_SECRET,
       redirectUris: [process.env.XERO_REDIRECT_URI],
-      scopes: 'openid profile email accounting.transactions accounting.contacts offline_access',
+      scopes: ['openid', 'profile', 'email', 'accounting.transactions', 'accounting.contacts', 'offline_access'],
     });
 
     return xero;
